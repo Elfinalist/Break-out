470ab4a3543c6a781f530c955f219950e90b4c9a
Kingsley <elfinalist@gmail.com>
2022-07-25 21:08:46 +0300

    drawing the block

---
+++ index.js
@@ -0,0 +1,8 @@
+
+//draw my block
+const grid = document.querySelector('.grid')
+const block = document.createElement('div')
+block.classList.add('block')
+block.style.left = '100px'
+block.style.bottom ='50px'
+grid.appendChild(block)
